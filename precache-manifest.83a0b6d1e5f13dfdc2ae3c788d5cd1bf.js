b8eba0bd5ae533f7d0154d27a2e6059a1e914967
Safal Gautam <safal.strym@gmail.com>
2023-04-16 00:18:53 +0545

    Updates

---
+++ precache-manifest.83a0b6d1e5f13dfdc2ae3c788d5cd1bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74c8940940985138c35bf64b8c29ae6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7aab59cf96d1aaaa061",
+    "url": "/static/css/2.f3d9550f.chunk.css"
+  },
+  {
+    "revision": "65042e8670b422507331",
+    "url": "/static/css/main.abf44639.chunk.css"
+  },
+  {
+    "revision": "b7aab59cf96d1aaaa061",
+    "url": "/static/js/2.f20aad32.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f20aad32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65042e8670b422507331",
+    "url": "/static/js/main.f7d0d5e3.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "f6cfd31c6e91ab8a2533fd5b23283985",
+    "url": "/static/media/bg.f6cfd31c.jpg"
+  }
+]);
